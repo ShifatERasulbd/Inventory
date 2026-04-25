9711d3c515c5796ceb475e48dcf499634fa4093e
unknown <shifaterasulbd@gmail.com>
2026-04-25 14:52:35 +0600

    lazy loader implemented

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,45 +1,50 @@
+import React, { Suspense, lazy } from 'react'
 import { LoginForm } from '@/components/login-form';
 import { Toaster } from '@/components/ui/sonner';
 import { AppProvider } from '@/context/AppContext';
 import AppLayout from '@/layouts/AppLayout';
 
-import Dashboard from '@/pages/dashboard';
+const Dashboard = lazy(() => import('@/pages/dashboard'));
 
-import Countries from '@/pages/Country/country';
-import AddContry from '@/pages/Country/addContry';
-import EditContry from '@/pages/Country/editContry';
+const Countries = lazy(() => import('@/pages/Country/country'));
+const AddContry = lazy(() => import('@/pages/Country/addContry'));
+const EditContry = lazy(() => import('@/pages/Country/editContry'));
 
-import States from '@/pages/State/states';
-import AddState from '@/pages/State/addState';
-import EditState from '@/pages/State/editState';
+const States = lazy(() => import('@/pages/State/states'));
+const AddState = lazy(() => import('@/pages/State/addState'));
+const EditState = lazy(() => import('@/pages/State/editState'));
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
     return (
         <AppProvider>
             <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
-                                <LoginForm />
-                            </main>
-                        }
-                    />
-                    <Route element={<AppLayout />}>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        {/* Countries List */}
-                        <Route path="/countries" element={<Countries />} />
-                        <Route path="/countries/add" element={<AddContry />} />
-                        <Route path="/countries/:id/edit" element={<EditContry />} />
+                <Suspense fallback={<div className="text-center p-10">Loading...</div>}>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
+                                    <LoginForm />
+                                </main>
+                            }
+                        />
 
-                        {/* State List */}
-                        <Route path="/states" element={<States />} />
-                        <Route path="/states/add" element={<AddState />} />
-                        <Route path="/states/:id/edit" element={<EditState />} />
-                    </Route>
-                </Routes>
+                        <Route element={<AppLayout />}>
+                            <Route path="/dashboard" element={<Dashboard />} />
+
+                            {/* Countries */}
+                            <Route path="/countries" element={<Countries />} />
+                            <Route path="/countries/add" element={<AddContry />} />
+                            <Route path="/countries/:id/edit" element={<EditContry />} />
+
+                            {/* States */}
+                            <Route path="/states" element={<States />} />
+                            <Route path="/states/add" element={<AddState />} />
+                            <Route path="/states/:id/edit" element={<EditState />} />
+                        </Route>
+                    </Routes>
+                </Suspense>
             </BrowserRouter>
             <Toaster position="top-right" richColors />
         </AppProvider>
